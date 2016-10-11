dfb6dd5f1845748fdb813a934d74e7d993a06e23
canhsiao <canhsiao@gmail.com>
2016-10-10 22:05:39 -0400

    some more update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,11 @@
 class HomeController < ApplicationController
 	def login
-		#  if(session[:user]!= nil)
-	   # respond_to do |format|
-	    #      format.html { redirect_to controller: 'bookings'}    
-	     # end
-	  #else
-	        #  session.clear
-  		#end
-    end
+	end
+
 	def logout
-        session.clear
-        @tempUser = User.new
-        session[:user] = @tempUser
-        render "login"
-    end
+		session.clear
+		@tempUser = User.new
+		session[:user] = @tempUser
+		render "login"
+	end
 end
